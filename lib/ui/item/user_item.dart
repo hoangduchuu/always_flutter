32d7fb20c8c95c3eb718f4612032226caf8b5451
Huu <hoangduchuuvn@gmail.com>
2024-05-22 15:09:02 +0700

    [DEV] update right click

--- lib/ui/item/user_item.dart
+++ lib/ui/item/user_item.dart
@@ -4,6 +4,7 @@ import 'package:developerfect_app/model/user_model.dart';
 import 'package:developerfect_app/route/navigators.dart';
 import 'package:developerfect_app/utils/log/my_logger.dart';
 import 'package:flutter/material.dart';
+import 'package:url_launcher/link.dart';
 
 class UserItem extends StatelessWidget {
   const UserItem({super.key, required this.user});
@@ -12,23 +13,29 @@ class UserItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return InkWell(
-      onTap: () {
-        MyLogger.d("USER: ${user.id}");
-        AppNavigators.gotoUserDetail(userId: '${user.id}');
+    return Link(
+      key: GlobalKey(),
+      uri: Uri.parse('${Uri.base.origin}/users/${user.id}'),
+      builder: (context, followLink) {
+        return InkWell(
+          onTap: () {
+            MyLogger.d("USER: ${user.id}");
+            AppNavigators.gotoUserDetail(userId: '${user.id}');
+          },
+          child: ListTile(
+            title: Text(user.name),
+            subtitle: Text(user.email),
+            leading: CircleAvatar(
+              child: 1 == 1
+                  ? Image.network(user.avatar)
+                  : Text(
+                      '${user.name[0].toUpperCase()}${user.name.split('').last[0].toUpperCase()}',
+                    ),
+            ),
+            trailing: const Icon(Icons.arrow_forward_ios),
+          ),
+        );
       },
-      child: ListTile(
-        title: Text(user.name),
-        subtitle: Text(user.email),
-        leading: CircleAvatar(
-          child: 1 == 1
-              ? Image.network(user.avatar)
-              : Text(
-                  '${user.name[0].toUpperCase()}${user.name.split('').last[0].toUpperCase()}',
-                ),
-        ),
-        trailing: const Icon(Icons.arrow_forward_ios),
-      ),
     );
   }
 }
